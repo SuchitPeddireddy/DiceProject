2ade9fa0a3383fdf1cff99032cea9ab82da80b52
SuchitPeddireddy <45395231+SuchitPeddireddy@users.noreply.github.com>
2018-12-05 09:07:13 -0500

    Dice
    A dice is initialized and it is rolled. You ca also pick the maount of sides.

---
+++ Dice.java
@@ -0,0 +1,36 @@
+public class Dice{
+
+	private int getValue;
+	private int sides;
+
+	public Dice(){
+		getValue = 6;
+		sides = 6;
+	}
+
+	public Dice(int sides){
+		this.sides = sides;
+		roll();
+	}
+
+	public int getDice(){
+		return getValue;
+	}
+
+	public void roll(){
+		int rand = (int)(Math.random()*6)+1;
+
+		getValue = rand;
+
+	}
+
+	public String toString(){
+		return "Number of sides : " + sides + ", Value : " + getValue;
+	}
+
+
+
+
+
+
+}
