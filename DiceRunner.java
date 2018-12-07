4b479cd657685db00769385a3efb2e336868677d
SuchitPeddireddy <45395231+SuchitPeddireddy@users.noreply.github.com>
2018-12-06 19:20:52 -0500

    DiceRunner

--- DiceRunner.java
+++ DiceRunner.java
@@ -1,9 +1,11 @@
+import java.util.ArrayList;
+
 public class DiceRunner{
 	public static void main(String[]args){
 
 		//Test 1
 
-			Dice dice1 = new Dice();
+			/*Dice dice1 = new Dice();
 			Dice dice2 = new Dice();
 			int counter = 0;
 
@@ -14,12 +16,31 @@ public class DiceRunner{
 
 				System.out.println("Roll #" + counter);
 				System.out.println("Dice 1 : " + dice1 + "\nDice 2 : " + dice2 + "\n");
-			}
+			}*/
+
 
 		//Test 2
 
-			for(int i = 0; i < diceHolder.addDie(); i++){
-				addDie
+			//ArrayList<Dice> diceCup = new ArrayList();
+			boolean start = true;
+			diceHolder dHolder = new diceHolder();
+			//int cntr = 1;
+			Dice d = null;
+			while(start){
+
+				d = new Dice();
+				int val = dHolder.addDie(d);
+
+				//System.out.println("dHolder:" + dHolder);
+
+				//System.out.println("val: " + val + ", Dice:" + d);
+				if(val == -1) { // DiceHolder is full
+
+					start = false;
+					dHolder.shake();
+
+					System.out.println("dHolder:" + dHolder);
+				}
 			}
 
 
